d85fdc91791100cd4fdfc91958faed1b629cdfe8
jorfran <jofra@uol.com.br>
2016-05-31 19:47:41 -0300

    Commit 01 da Video Aula 05

---
+++ src/main/java/clienteweb/ClienteServlet.java
@@ -0,0 +1,44 @@
+package clienteweb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+/*
+ * @WebServlet configura o padrão da url , como
+ * este servlet será chamado pelo navegador
+ */
+@WebServlet(urlPatterns={"/cliente","/clienteServlet","/clienteController"})
+@SuppressWarnings("serial")
+/*
+ * ClienteServlet objeto java capaz de receber
+ * requisições vindas da web
+ */
+public class ClienteServlet extends HttpServlet{
+@Override
+ protected void doGet(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
+ //super.doGet(req, resp);
+	 //System.out.println("Chamou pelo método get");
+	 resp.setCharacterEncoding("UTF-8");
+	 resp.getWriter().print("Chamou pelo método GET !");	 
+}
+ @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	//System.out.println("Chamou pelo método POST");
+	 resp.setCharacterEncoding("UTF-8");
+	 resp.getWriter().print("Chamou pelo método POST !");
+	}
+ @Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 resp.setCharacterEncoding("UTF-8");
+	 resp.getWriter().print("Chamou pelo método DELETE !");	
+	}
+ @Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 resp.setCharacterEncoding("UTF-8");
+	 resp.getWriter().print("Chamou pelo método PUT !");	
+	}
+}
